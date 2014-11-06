330901d99aec265d2cc364fb601b3a159acbf467
Luke Vivier <luke@vivier.ca>
2014-11-06 15:30:08 -0800

    add Media and OpenHouse schemas

--- index.js
+++ index.js
@@ -6,6 +6,8 @@
 var property = require('./schemas/property');
 var office = require('./schemas/office');
 var member = require('./schemas/member');
+var media = require('./schemas/media');
+var openhouse = require('./schemas/openhouse');
 
 /**
  * Expose Schemas
@@ -16,6 +18,8 @@ module.exports = {
   property: property,
   office:   office,
   member:   member,
+  media:    media,
+  openhouse:openhouse,
   // export aliases
   listing:  property,
   agent:    member
